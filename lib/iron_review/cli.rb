8f1c4e276b0035f9af569ac3091d669c395ddc6b
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2018-08-08 00:34:22 +0000

    Corrected name and url path.

--- lib/iron_review/cli.rb
+++ lib/iron_review/cli.rb
@@ -1,6 +1,6 @@
 class IronReview::CLI
   def start
-    puts "Welcome to your curling iron review!"
+    puts "Welcome to your curling iron list!"
     puts ""
 
   IronReview::Scraper.scrape_irons
@@ -10,17 +10,19 @@ class IronReview::CLI
   def list_irons
     puts "Here is the list of the best reviewed curling irons."
     puts ""
+    puts "Select an iron for the product description:"
+    puts ""
     irons = IronReview::Iron.all
     irons.each.with_index(1).each do |iron, index|
-      puts "#{index}. #{iron.name}"
+    puts "#{index}. #{iron.name}"
+    select_irons
     end
   end
 
   def select_irons
-    puts "Select an iron for the product description:"
     input = gets.strip
     index = input.to_i -1
     iron = IronReview::Iron.all[index]
     IronReview::Scraper.scrape_details(iron)
+    end
   end
-end
--- lib/iron_review/scraper.rb
+++ lib/iron_review/scraper.rb
@@ -4,14 +4,17 @@ BASE_URL = "https://www.beautybrands.com/hair-bath-skincare-makeup/reviews-tools
 
   def self.scrape_irons
     doc = Nokogiri::HTML(open(BASE_URL))
-    doc.css(".grid").each do |iron_doc|
-      name = iron_doc.css(".grid a").attribute("title").text
-      url = iron_doc.css(".grid a").attribute("href").text
-      binding.pry
-      IronReview::Iron.new(name, url)
+    doc.css(".grid li").each do |iron_doc|
+    name = iron_doc.css(".itemName").text
+    url = iron_doc.css(".detailImageWrapper").attribute("href").value
+    IronReview::Iron.new(name, url)
     end
+    binding.pry
   end
 
   def self.scrape_details(iron)
+    doc = Nokogiri::HTML(open(BASE_URL))
+    doc.css(".ml-product-desc-short").each do |details_doc|
   end
+end
 end
